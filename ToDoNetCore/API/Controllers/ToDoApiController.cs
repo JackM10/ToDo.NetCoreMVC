1992224ee367c6efb8560cee46b316f6049d39e5
JackM10 <neo111@mail.ru>
2018-12-23 17:57:12 +0200

    Added API controller for Angular frotnt-end, DB moved to docker

---
+++ ToDoNetCore/API/Controllers/ToDoApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoNetCore.Models;
+
+namespace ToDoNetCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoApiController : Controller
+    {
+        #region Fields
+
+        private IToDoRepository _context;
+
+        #endregion
+
+        #region Constructors
+
+        public ToDoApiController(IToDoRepository context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region API Methods
+
+        [HttpGet]
+        public async Task<List<ToDoModel>> List() => await _context.ToDo.AsNoTracking().ToListAsync();
+
+        #endregion
+    }
+}
